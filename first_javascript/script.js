df928109d591e5a6bcf07c2c8489dd0fb20d2413
Joseph <jhl843@nyu.edu>
2018-11-08 11:20:15 -0500

    reupload

---
+++ first_javascript/script.js
@@ -0,0 +1,76 @@
+// send any messages to the developer's console
+
+//console.log('Helllo!!!')
+
+// defining variables
+
+//a variable that contains a string
+
+//var firstName = 'Joseph'
+//var lastName = 'Lee'
+//
+////string concatenation
+//console.log (firstName + ' ' + lastName);
+
+
+//var num1 = 5
+//var num2 = 5.4
+
+//console.log (num1 + num2)
+
+//arrays in JS can have mixed data types
+//var firstArray = ['bert', 'ernie', 'big bird', 'snuffie']
+//console.log (firstArray);
+
+//arrays are indexed
+//console.log(firstArray[0]);
+//
+//var secondArray = ['kermit', 'miss piggy', 'gonzo', 'swedish chef']
+//
+////multidimensional array
+//var thirdArray = [firstArray, secondArray];
+//
+//console.log(thirdArray[1][2]);
+//
+//
+//
+////set up control flow
+////if and else statements
+//
+//var num3 = 10
+//
+//if (num3 === 14) {
+//    console.log('your number is the correct number')
+//} else if (num3 > 10) {
+//    console.log('your number is too big')
+//} else {console.log('you did not choose the correct number')}
+
+//function are operations or methods
+
+//function addNumber(numberOne, numberTwo) {
+//    return numberOne + numberTwo // return exists the function
+//}
+//
+//console.log(addNumber(40, 60))
+
+
+//create a function that is a virtual door
+// if the user goes through the tacos door, give them a message about delicious tacos
+//if the user goes through the pizza door, give them a message about yummy pizza
+//if the user goes through the noodles door, give them a message about awesome noodles
+
+//function foodDoor(food) {
+//    if (food === 'tacos') {
+//        return "delicious tacos" }
+//        else if (food === 'pizza') {return "yummy pizza"}
+//            else if (food === 'noodles') {
+//                return "awesome noodles" }
+//    else return "what are you eating?"
+//}
+//
+//console.log(foodDoor('tacos'))
+//console.log(foodDoor('pizza'))
+//console.log(foodDoor('noodles'))
+//console.log(foodDoor('rice'))
+
+
