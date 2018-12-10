f5e8fb29e49f8fe4494e19a70bf9f67bc80ae9da
Joseph <jhl843@nyu.edu>
2018-12-10 18:07:36 -0500

    text change

--- rainbow/script.js
+++ rainbow/script.js
@@ -3,10 +3,14 @@ $(document).ready(function(){
   	var scroll = $(window).scrollTop();
 	  if (scroll > 100) {
 	    $(".mainmenu").css("background-color" , "rgb(192,217,51, 0.95)");
+          $("#icon").css("width", "100px");
+          $(".mainmenu li").css('font-size', '17px');
 	  }
 
 	  else{
-		  $(".mainmenu").css("background-color" , "transparent");  	
+		  $(".mainmenu").css("background-color" , "transparent"); 
+          $("#icon").css("width", "150px");
+          $(".mainmenu li").css('font-size', '20px');
 	  }
   })
 })
